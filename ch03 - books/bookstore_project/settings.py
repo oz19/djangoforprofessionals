dcbe115e2011f0925603910797b5fa93227d31e8
G.Ocon <gurutz.ocon@gmail.com>
2020-01-14 18:17:51 +0100

    ch8

--- ch03 - books/bookstore_project/settings.py
+++ ch03 - books/bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8n3q8n43qvpoh&k=3z(4af^ig@dxgvpb0snz37($9*0n0+9%fa'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 

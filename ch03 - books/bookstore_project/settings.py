ad545cb9f4a4381f22a1a56ae9dd3538d5348071
G.Ocon <gurutz.ocon@gmail.com>
2020-01-17 16:14:47 +0100

    ch16

--- ch03 - books/bookstore_project/settings.py
+++ ch03 - books/bookstore_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'allauth',
     'allauth.account',
+    'debug_toolbar',
 
     # Local
     'users.apps.UsersConfig',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,7 +64,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware'
 ]
+
+CACHE_MIDDLEWARE_ALIAS      = 'default'
+#CACHE_MIDDLEWARE_SECONDS    = 604800
+CACHE_MIDDLEWARE_SECONDS    = 30
+CACHE_MIDDLEWARE_KEY_PREFIX = ''
 
 ROOT_URLCONF = 'bookstore_project.urls'
 
@@ -184,3 +193,8 @@ ACCOUNT_UNIQUE_EMAIL = True
 # Stripe
 STRIPE_TEST_PUBLISHABLE_KEY = os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
 STRIPE_TEST_SECRET_KEY      = os.environ.get('STRIPE_TEST_SECRET_KEY')
+
+# django-debug-toolbar
+import socket
+hostname, _, ips    = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS        = [ip[:-1] + "1" for ip in ips]
